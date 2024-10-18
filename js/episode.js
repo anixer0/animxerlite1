d4f1c07ea08384ab9c369d1a19f07f131157c344
anixer0 <anixer42@gmail.com>
2024-10-18 16:21:50 +0800

    Update episode.js

--- js/episode.js
+++ js/episode.js
@@ -7,7 +7,7 @@ const dlapi = "/download/";
 
 // Api Server Manager
 
-const AvailableServers = ["anixer-api.anixer42.workers.dev"];
+const AvailableServers = ["https://anixer-api.anixer42.workers.dev"];
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
